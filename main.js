4ec1c5e48a62e9a79e213156720ffa0abe69dfb7
Shealyn Sugai <shealyn@student.dev-island.org>
2024-01-16 20:52:19 -1000

    space change

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ import { createTodoItemView } from "./view/todoItem-view.js";
 
 let item = createTodoItem("Get Pizza", "cheese");
 console.log(item.getTitle());
-// item.setDescription("cheese");
 
 createTodoItemView(item.getTitle(), item.getDescription());
